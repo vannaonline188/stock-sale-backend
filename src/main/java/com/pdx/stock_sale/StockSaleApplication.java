e8f84e867c705df4fd1e60b29c333c2d1735f70f
vannaonline188 <vannaonline188@gmail.com>
2025-03-31 21:31:04 +0700

    first_starter

---
+++ src/main/java/com/pdx/stock_sale/StockSaleApplication.java
@@ -0,0 +1,13 @@
+package com.pdx.stock_sale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockSaleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockSaleApplication.class, args);
+	}
+
+}
